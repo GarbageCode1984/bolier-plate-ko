bcb06f575ebdf3729791bd32ff23dc81a863ab1c
GarbageCode1984 <rkqlwl1984@gmail.com>
2022-06-28 20:04:22 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require("./models/User")
+const config = require('./config/key')
+
+
+//application/x-www-from-urlencoded
+app.use(express.urlencoded({extended: true}))
+//application/json
+app.use(express.json())
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dntjr:djqlf0595!@cluster0.qesvhwr.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    //회원 가입할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {  
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
